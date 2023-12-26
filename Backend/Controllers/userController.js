36deb4811d88a8d70ea0db5c93b97d912bd26f76
Murfadahmed <malikmurfad361@gmail.com>
2023-12-26 19:28:50 +0500

    changes

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -8,7 +8,7 @@ export const addUserController = async (req, res) => {
     try {
         // console.log(newUser);
         console.log(req.body);
-        await newUser.save()
+        // await newUser.save()
         res.status(201)
     } catch (error) {
         res.status(401).json("yahn controller may line 12 error aya hai",{ message: error.message })
--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -7,8 +7,9 @@ export const addUser = async (data) => {
     try {
         console.log('API hit successful'); // Ensure this log is displayed
         const response = await axios.post(`${URL}/add`, data);
-        console.log('Response:', response.data); // Log the response for debugging
-        return response.data; // Return the response data
+        console.log(response);
+        // console.log('Response:', response.data); // Log the response for debugging
+        // return response.data; // Return the response data
     } catch (error) {
         console.error('Error adding user:', error); 
         throw error; // Re-throw the error to handle it in the calling code
